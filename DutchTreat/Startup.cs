04f18e792d4953146e13c3df7be0ce0751726a8b
Vasyl Volyk <vasiliy.volik@gmail.com>
2017-12-21 12:05:28 +0800

    HTML tags added

--- DutchTreat/Startup.cs
+++ DutchTreat/Startup.cs
@@ -27,7 +27,7 @@ namespace DutchTreat
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("<html><body><h1>Hello World!</h1></body></h1>");
             });
         }
     }
