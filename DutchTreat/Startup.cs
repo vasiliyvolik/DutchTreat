ba8e01fe5bf6a939990686a4c96e7f7146c0a8b3
Vasyl Volyk <vasiliy.volik@gmail.com>
2017-12-21 11:52:45 +0800

    IsDevelopment deleted

--- DutchTreat/Startup.cs
+++ DutchTreat/Startup.cs
@@ -20,10 +20,10 @@ namespace DutchTreat
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+  //          if (env.IsDevelopment())
+  //          {
+  //              app.UseDeveloperExceptionPage();
+  //          }
 
             app.Run(async (context) =>
             {
