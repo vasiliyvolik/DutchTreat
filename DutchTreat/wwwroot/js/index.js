6621f72a3eceadc6a1325c23055e9f0475a494ca
Vasyl Volyk <vv@sitecore.net>
2018-01-08 10:19:17 +0800

    practical JQuery

--- DutchTreat/wwwroot/js/index.js
+++ DutchTreat/wwwroot/js/index.js
@@ -1,32 +1,44 @@
-﻿var x = 0;
-var s = "";
+﻿$(document).ready(function () {
+    var x = 0;
+    var s = "";
 
-console.log("Hello Pluralsight");
-
-// JQuery
-
-//var theForm = document.getElementById("theForm");
-//theForm.hidden = true;
-
-var theForm = $("#theForm");
-theForm.hide();
-
-//var button = document.getElementById("buyButton");
-//button.addEventListener("click", function () {
-//    alert("Buying item");
-//})
-
-var button = $("#buyButton");
-button.on("click", function () {
-    console.log("Buying item");
-})
+    console.log("Hello Pluralsight");
 
 
-//var productinfo = document.getElementsByClassName("product-props");
-//var listitems = productinfo.item[0].children;
+    // JQuery
 
-var productinfo = $(".product-props li");
-productinfo.on("click", function() {
-    console.log("you clicked on " + $(this).text());
-}
-);
+    //var theForm = document.getElementById("theForm");
+    //theForm.hidden = true;
+
+    var theForm = $("#theForm");
+    theForm.hide();
+
+    //var button = document.getElementById("buyButton");
+    //button.addEventListener("click", function () {
+    //    alert("Buying item");
+    //})
+
+    var button = $("#buyButton");
+    button.on("click", function () {
+        console.log("Buying item");
+    })
+
+
+    //var productinfo = document.getElementsByClassName("product-props");
+    //var listitems = productinfo.item[0].children;
+
+    var productinfo = $(".product-props li");
+    productinfo.on("click", function () {
+        console.log("you clicked on " + $(this).text());
+    }
+    );
+
+    var $loginToggle = $("#loginToggle");
+    var $popupForm = $(".popup-form");
+
+    $loginToggle.on("click", function () {
+        $popupForm.fadeToggle(1000);
+    })
+
+
+});
