0f61bc7d670fc0968eae39a3379975a1d8803136
Vasyl Volyk <vv@sitecore.net>
2018-01-09 11:14:10 +0800

    Using Tag Helpers

--- DutchTreat/Controllers/AppController.cs
+++ DutchTreat/Controllers/AppController.cs
@@ -15,6 +15,7 @@ namespace DutchTreat.Controllers
             return View();
         }
 
+        [HttpGet("contact")]
         public IActionResult Contact()
         {
             ViewBag.Title = "Contact Us";
