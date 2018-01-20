79c3f1a427fd897788e004c519d5bf2b206a4434
Vasyl Volyk <vv@sitecore.net>
2018-01-20 13:03:17 +0800

    Using Entity Framework Core tooling

--- DutchTreat/Startup.cs
+++ DutchTreat/Startup.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DutchTreat.Data;
 using DutchTreat.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace DutchTreat
@@ -16,6 +18,10 @@ namespace DutchTreat
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<Data.DutchContext>(cfg =>
+            {
+                cfg.UseSqlServer("server=.;");
+            });
             services.AddTransient<IMailService, NullMailService>();
             services.AddMvc();
         }
