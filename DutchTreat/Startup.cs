8c868b7b78a8659b3506179d16eaa5e8ca77fbc8
Vasyl Volyk <vv@sitecore.net>
2018-01-24 09:27:59 +0800

    Using DbContext

--- DutchTreat/Startup.cs
+++ DutchTreat/Startup.cs
@@ -8,19 +8,25 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace DutchTreat
 {
     public class Startup
     {
+        private readonly IConfiguration _config;
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<Data.DutchContext>(cfg =>
             {
-                cfg.UseSqlServer("server=.;");
+                cfg.UseSqlServer(_config.GetConnectionString("DutchConnectionString"));
             });
             services.AddTransient<IMailService, NullMailService>();
             services.AddMvc();
